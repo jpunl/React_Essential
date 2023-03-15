0b39e2039e74332489ef118f1d7c9e21d16b767b
jpunl <s-jpu2@unl.edu>
2023-03-15 13:47:47 +1100

    complete 04_04

--- Ch04/04_04/start/react-app/src/App.js
+++ Ch04/04_04/start/react-app/src/App.js
@@ -1,15 +1,11 @@
 import "./App.css";
+import { useState } from "react";
 
-const [firstCity, second] = [
-  "Tokyo",
-  "Tahoe City",
-  "Bend"
-];
 
-console.log(firstCity);
-console.log(second);
 
 function App({ library }) {
+  const what = useState();
+  console.log(what);
   return (
     <div className="App">
       <h1>Hello from {library}</h1>
