9a98ab48839280e03dab1a898e76ce9794208a85
jpunl <s-jpu2@unl.edu>
2023-03-15 22:55:05 +1100

    complete 07_01

--- Ch07/07_01/start/react-app/src/App.js
+++ Ch07/07_01/start/react-app/src/App.js
@@ -1,38 +1,23 @@
 import "./App.css";
 
-const tahoe_peaks = [
-  { name: "Freel", elevation: 10891 },
-  { name: "Monument", elevation: 10067 },
-  { name: "Pyramid", elevation: 9983 },
-  { name: "Tallac", elevation: 9735 }
-];
-
-function List({ data, renderItem, renderEmpty }) {
-  return !data.length ? (
-    renderEmpty
-  ) : (
-    <ul>
-      {data.map((item) => (
-        <li key={item.name}>
-          {renderItem(item)}
-        </li>
-      ))}
-    </ul>
+function Home() {
+  return (
+    <div>
+      <h1>My Website</h1>
+    </div>
   );
 }
 
+function About() {
+  return <h1>About Us</h1>;
+}
+
+function Contact() {
+  return <h1>Contact Us</h1>;
+}
+
 function App() {
-  return (
-    <List
-      data={tahoe_peaks}
-      renderEmpty={<p>This list is empty</p>}
-      renderItem={(item) => (
-        <>
-          {item.name} - {item.elevation} ft.
-        </>
-      )}
-    />
-  );
+  return <Home />;
 }
 
 export default App;
