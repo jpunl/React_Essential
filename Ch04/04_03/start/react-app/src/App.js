fac4209cc9374bbf53790e6e745ae92097392f56
jpunl <s-jpu2@unl.edu>
2023-03-15 13:46:42 +1100

    complete 04_03

--- Ch04/04_03/start/react-app/src/App.js
+++ Ch04/04_03/start/react-app/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 
-function App() {
+const [firstCity] = ["Tokyo", "Beijing", "NewYork"];
+
+console.log(firstCity);
+
+function App({library}) {
   return (
     <div className="App">
-      <h1>Hello from React</h1>
+      <h1>Hello from {library}</h1>
     </div>
   );
 }
